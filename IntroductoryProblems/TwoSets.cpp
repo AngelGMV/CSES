9168f689b5f1bfccb8731408d83479ab68eccca5
Amuve <angel_ga_munoz@hotmail.com>
2025-05-08 21:45:03 -0700

    Hola

--- IntroductoryProblems/TwoSets.cpp
+++ IntroductoryProblems/TwoSets.cpp
@@ -11,17 +11,36 @@ using vi = vector<int>;
 #define ROF(i, a, b) for (int i = (int)a - 1; i >= (int)b; --i)
 #define ENDL '\n'
 
-
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin >> n;
-    int suma = n*(n + 1)/2/2;
-    if (suma%2!=0) {
+
+    ll total = 1LL * n * (n + 1) / 2;
+    if (total % 2 != 0) {
         cout<<"NO"<<ENDL;
         return 0;
     }
+
+    ll target = total / 2;
+    vi v1, v2;
+    for (int i = n; i >= 1; --i) {
+        if (target >= i) {
+            v1.pb(i);
+            target -= i;
+        } else {
+            v2.pb(i);
+        }
+    }
+
+    cout << "YES" << ENDL;
+    cout << v1.size() << ENDL;
+    for (int x : v1) cout << x << " ";
+    cout << ENDL;
+    cout << v2.size() << ENDL;
+    for (int x : v2) cout << x << " ";
+    cout << ENDL;
 
     return 0;
 }
