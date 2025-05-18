a6d15c907db175ecbe9d0b080c1927219bb32e5d
Amuve <angel_ga_munoz@hotmail.com>
2025-05-17 20:39:20 -0700

    ✔️

---
+++ IntroductoryProblems/PalindromeReorder.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define pb push_back
+#define SZ(x) ((int)(x).size())
+#define ALL(x) begin(x), end(x)
+#define FOR(i, a, b) for (int i = (int)a; i < (int)b; ++i)
+#define ROF(i, a, b) for (int i = (int)a - 1; i >= (int)b; --i)
+#define ENDL '\n'
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    map<char, int> mp;
+    string s;
+    cin >> s;
+    for (char c : s) {
+        mp[c]++;
+    }
+    FOR(i, 0, SZ(s)) {
+
+    }
+
+    return 0;
+}
